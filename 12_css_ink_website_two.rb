4b9395618ee77f4d19446dd80c3e0bf9575f0ce1
Flynn <matt.etherington@coderacademy.edu.au>
2018-08-28 13:28:19 +1000

    Initial commit - 12_css_ink_website_two

---
+++ 12_css_ink_website_two.rb
@@ -0,0 +1,9 @@
+# The Company is happy with the ink sales, and wants to collect data on their customers.
+# They want a ‘sign up’ page that has a form collecting the following information:
+# - name
+# - email
+# - DOB
+# - gender (with a radio button)
+# - State (from a drop down menu - so look up the select element)
+# - a checkbox that says they are happy for you to send them emails about special deals etc
+# - and a submit button
